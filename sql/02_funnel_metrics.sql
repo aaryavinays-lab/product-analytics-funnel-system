89029bf17c3d89d433ea9762ffee1ae311bc5c62
aaryavinays-lab <aaryavinays@gmail.com>
2026-03-14 07:38:36 -0400

    Update 02_funnel_metrics.sql

--- sql/02_funnel_metrics.sql
+++ sql/02_funnel_metrics.sql
@@ -1,1 +1,5 @@
-
+SELECT
+   SUM(CASE WHEN event_type = 'view' THEN 1 ELSE 0 END) AS views,
+   SUM(CASE WHEN event_type = 'cart' THEN 1 ELSE 0 END) AS carts,
+   SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS purchases
+FROM Events;
