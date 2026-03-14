e5deacbd32ff35f3f6833400e02d21e222434e57
aaryavinays-lab <aaryavinays@gmail.com>
2026-03-14 09:12:21 -0400

    Update Conversion_rates.sql

--- sql/Conversion_rates.sql
+++ sql/Conversion_rates.sql
@@ -1,1 +1,21 @@
+SELECT
+    SUM(CASE WHEN event_type = 'view' THEN 1 ELSE 0 END) AS views,
+    SUM(CASE WHEN event_type = 'cart' THEN 1 ELSE 0 END) AS carts,
+    SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS purchases,
 
+    SUM(CASE WHEN event_type = 'cart' THEN 1 ELSE 0 END) * 1.0
+    /
+    SUM(CASE WHEN event_type = 'view' THEN 1 ELSE 0 END)
+    AS view_to_cart_conversion,
+
+    SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) * 1.0
+    /
+    SUM(CASE WHEN event_type = 'cart' THEN 1 ELSE 0 END)
+    AS cart_to_purchase_conversion,
+
+    SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) * 1.0
+    /
+    SUM(CASE WHEN event_type = 'view' THEN 1 ELSE 0 END)
+    AS view_to_purchase_conversion
+
+FROM Events;
