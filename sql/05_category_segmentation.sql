b81a6334e7feb6f3c301eae401e4053dc4e35e80
aaryavinays-lab <aaryavinays@gmail.com>
2026-03-21 09:38:45 -0400

    Update 05_category_segmentation.sql

--- sql/05_category_segmentation.sql
+++ sql/05_category_segmentation.sql
@@ -1,1 +1,15 @@
+SELECT 
+    category_code,
 
+    COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END) AS users_viewed,
+
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS users_purchased,
+
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) * 1.0 /
+    NULLIF(COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END), 0) AS conversion
+
+FROM Events
+
+GROUP BY category_code
+
+ORDER BY conversion DESC;
