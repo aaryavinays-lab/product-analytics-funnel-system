f282089944f59dc5028cbca60bb34e770537161d
aaryavinays-lab <aaryavinays@gmail.com>
2026-03-21 09:27:52 -0400

    Update 04_daily_funnel.sql

--- sql/04_daily_funnel.sql
+++ sql/04_daily_funnel.sql
@@ -1,9 +1,9 @@
 SELECT 
     DATE(event_time) AS event_date,
 
-    COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END) AS viewed,
+    COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END) AS users_viewed,
 
-    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS purchased,
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS users_purchased,
 
     COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) * 1.0 /
     NULLIF(COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END), 0) AS conversion
